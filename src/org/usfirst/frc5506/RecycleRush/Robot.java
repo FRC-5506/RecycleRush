0022899a397819f88ed51716f02bb14b0954bd6d
nh-99 <ifence9@gmail.com>
2015-01-30 08:31:13 -0500

    Update arms & robot

--- src/org/usfirst/frc5506/RecycleRush/Robot.java
+++ src/org/usfirst/frc5506/RecycleRush/Robot.java
@@ -19,13 +19,6 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import org.usfirst.frc5506.RecycleRush.commands.*;
 import org.usfirst.frc5506.RecycleRush.subsystems.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
 
     Command autonomousCommand;
